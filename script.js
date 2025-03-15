820fa7f230dff86211bbcc1ecac78450b6fd5715
sudipskr321 <sudipskr321@gmail.com>
2025-03-15 13:19:55 +0530

    added project , contact container

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const skill = (skills) => {
   document.getElementById(skills).scrollIntoView({ behavior: "smooth" });
 };
+
+const project = (projects) => {
+  document.getElementById(projects).scrollIntoView({ behavior: "smooth" });
+}
+
+const contact = (contacts) => {
+  document.getElementById(contacts).scrollIntoView({ behavior: "smooth" });
+};
+
